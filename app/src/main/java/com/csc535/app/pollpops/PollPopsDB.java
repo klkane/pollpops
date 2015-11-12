96278441476f65f3be7b13f2ae8ce612fe861daa
Kevin L. Kane <kevin.kane@gmail.com>
2015-11-11 20:43:15 -0500

    first commit

---
+++ app/src/main/java/com/csc535/app/pollpops/PollPopsDB.java
@@ -0,0 +1,46 @@
+package com.csc535.app.pollpops;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+
+import java.util.Set;
+
+public class PollPopsDB {
+    private MongoDatabase getDB( String pid ) {
+        MongoClient mongoClient = new MongoClient( "66.175.214.36" , 27107 );
+        MongoDatabase db = mongoClient.getDatabase(pid);
+        return db;
+    }
+    public void sendChatMessage(String msg, String performance_id ) {
+        MongoDatabase db = this.getDB(performance_id);
+        MongoCollection<Document> coll = db.getCollection("chat_messages");
+        Document doc = new Document("message", msg)
+                .append("user", "testuser")
+                .append("time", "blorp");
+        coll.insertOne(doc);
+    }
+
+    public void createPerformance( String pid, String performer, String location, String datetime ) {
+        MongoDatabase db = this.getDB(pid);
+        MongoCollection<Document> coll = db.getCollection("performance");
+        Document doc = new Document("location", location)
+                .append("datetime", datetime)
+                .append("performer", performer);
+        coll.insertOne(doc);
+    }
+
+    public void createSetList( String pid, String[] songs ) {
+        MongoDatabase db = this.getDB(pid);
+        MongoCollection<Document> coll = db.getCollection("setlist");
+        for( String set : songs ) {
+            Document doc = new Document("song", set);
+            coll.insertOne(doc);
+        }
+    }
+    public String getMessages( String vid ) {
+        return "blorp";
+    }
+}
+
---
+++ app/src/main/java/com/csc535/app/pollpops/WelcomeActivity.java
@@ -0,0 +1,45 @@
+package com.csc535.app.pollpops;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.MenuItem;
+import android.content.Intent;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    public void enterPerformance(View v) {
+        Intent intent = new Intent(getApplicationContext(), UserPerformanceView.class);
+        PollPopsDB pdb = new PollPopsDB();
+        pdb.sendChatMessage( "blorp", "AD12345" );
+        startActivity(intent);
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_welcome, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
